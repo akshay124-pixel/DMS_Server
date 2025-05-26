3cd8f715143ce44aac5d0e4e5e624e6f3ea022f3
Akshay Pratap Singh <akshay124-pixel@example.com>
2025-05-26 14:57:19 +0530

    Done

--- Schema/Model.js
+++ Schema/Model.js
@@ -9,10 +9,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    match: [
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Please fill a valid email address",
-    ],
   },
   password: {
     type: String,
